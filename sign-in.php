46b194b76a3cf40bce73b099bd72d6299e298cb6
Anis Dhapa <anis.dhapa@sfgov.org>
2019-04-15 07:42:54 -0700

    sign-in and sign-up webpage

---
+++ sign-in.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anisdhapa
+ * Date: 2019-04-14
+ * Time: 20:55
+ */
+
+
+echo <<<_END
+
+
+<div class="d-flex justify-content-center align-content-center">
+      <form action="action/signin" method="post" class="form-signin" onsubmit="return validate(this, '#errorMessage')"
+novalidate>
+        <h2 class="form-signin-heading">Sign In Page</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="Email address" autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="inputPassword" id="inputPassword" class="form-control" placeholder="Password">
+        <div class="checkbox">
+          <label>
+            <input type="checkbox" name="inputRemember" value="remember-me"> Remember me
+          </label>
+        </div>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+</div>
+_END;
---
+++ sign-up.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anisdhapa
+ * Date: 2019-04-15
+ * Time: 07:38
+ */
+
+echo <<<_END
+
+
+<div class="d-flex justify-content-center align-content-center">
+      <form action="action/signup" method="post" class="form-sign-up" onsubmit="return validate(this, '#errorMessage')"
+novalidate>
+        <h2 class="form-signin-heading">SIGN UP PAGE</h2>
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="inputEmail" id="inputEmail" class="form-control" placeholder="Email address" autofocus>
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="inputPassword" id="inputPassword" class="form-control" placeholder="Password">
+      
+        <button class="btn btn-lg btn-primary btn-block" type="submit">SIGN UP</button>
+      </form>
+</div>
+_END;
